c93340c285af697bac29512eefa73519ecbee3c9
SYugender <yugender9908@gmail.com>
2024-09-02 14:30:22 +0530

    first commit

--- Auth_Service/Auth_Service/src/main/java/com/wipro/repository/UserCredentialRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wipro.entity.UserCredential;
-
-public interface UserCredentialRepository extends JpaRepository<UserCredential, Integer> {
-    Optional<UserCredential> findByName(String username);
-}
